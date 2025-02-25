4d9fcaeaa500024fc3da2e45154bf5beb873eaf2
francis <francisreybetonio46@gmail.com>
2025-02-25 13:47:21 +0800

    successfully connected to mysql

---
+++ server/src/config/dbconfig.js
@@ -0,0 +1,61 @@
+import mysql from 'mysql2/promise';
+//import "dotenv/config";
+
+// environment variables
+//const HOST = process.env.DB_HOST;
+//const USER = process.env.DB_USER;
+//const DATABASE = process.env.DB_NAME
+
+// dtabase connection pool
+const pool = mysql.createPool({
+    host: 'localhost',                                 // host
+    user: 'francis',                                // database user
+    password: 'francisrey123',
+    database: 'ponder',                         // database name
+    waitForConnections: true,                   
+    connectionLimit: 10,                        // max connections at a time
+    maxIdle: 10,                                // max idle connections
+    idleTimeout: 60000,                          
+    queueLimit: 0,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 0,
+});
+
+export default pool;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
